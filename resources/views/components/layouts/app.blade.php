196b1b80c8133b5b1146cc25baef4487a28d58f3
Robert <robertfrancisti@hotmail.com>
2025-07-24 18:18:58 +0200

    remove logo

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -1,7 +1,7 @@
 <x-layouts.app.plain :title="$title ?? null">
-    <div class="absolute top-4 left-4">
-        <img src="{{ asset('images/dicalist_logo.png') }}" alt="Dicalist Logo" class="h-32 w-auto" />
-    </div>
+    <!-- <div class="absolute top-4 left-4"> -->
+    <!--     <img src="{{ asset('images/dicalist_logo.png') }}" alt="Dicalist Logo" class="h-32 w-auto" /> -->
+    <!-- </div> -->
     <flux:main>
         {{ $slot }}
     </flux:main>
